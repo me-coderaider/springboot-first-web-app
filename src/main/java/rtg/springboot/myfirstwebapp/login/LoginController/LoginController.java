11938ca66bce6e56e749d57de3869accca4862ed
me-coderaider <kashyappankajnsp@gmail.com>
2024-07-06 16:10:10 +0530

    Added Logging and different levels of it.

--- src/main/java/rtg/springboot/myfirstwebapp/login/LoginController/LoginController.java
+++ src/main/java/rtg/springboot/myfirstwebapp/login/LoginController/LoginController.java
@@ -1,5 +1,7 @@
 package rtg.springboot.myfirstwebapp.login.LoginController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	
+	private Logger logger =LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
 	public String goToLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
-		System.out.println("Request Params is "+name); // NOT RECOMMENDED FOR PROD CODE
+		logger.debug("Request Params is {}", name);
+		logger.info("I want this to printed at info level");
+		logger.warn("I want this to printed at warn level");
 		return "login";
 	}
 }
